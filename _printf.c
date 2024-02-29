8332341b4344bad85b62df9b9aea9524da5d9ab0
AllenKeelig <allen.keeling@atlasschool.com>
2024-02-29 21:43:22 +0000

    AllenFailedAt%checks

--- _printf.c
+++ _printf.c
@@ -6,16 +6,28 @@
 int _printf(const char *format, ...)
 {
 	size_t i, len = strlen(format);
-       char *strarray;
+	char *strarray;
+	struct spec spec;
 
-       strarray = malloc(sizeof(char) * (len + 1));
+	strarray = malloc(sizeof(char) * (len + 1));
 
-       for (i = 0; i < len; i++)
-       {
-	       strarray[i] = format[i];
-	       _putchar(strarray[i]);
-       }
+	for (i = 0; i < len; i++)
+	{
+		strarray[i] = format[i];
+		if (strarray[i] != '%')
+		_putchar(strarray[i]);
+		else
+		{
+			spec.specifier = strarray[i+1];
+			if (spec.specifier == 's')
+				printf("this is a string\n"); /*check to see if the format detection is working*/
+			if (spec.specifier == 'c')
+				printf("this is a characrer\n"); /*check to see if the format detection is working*/
 
-       free(strarray);
-       return (len);
+		}
+
+	}
+
+	free(strarray);
+	return (len);
 }
--- _printf.h
+++ _printf.h
@@ -1,6 +1,10 @@
 #ifndef MY_HEADER_FILE_H
 #define MY_HEADER_FILE_H
 
+struct spec
+{
+	char specifier;
+};
 int _putchar(char c);
 int _printf(const char *format, ...);
 
